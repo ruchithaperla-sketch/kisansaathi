7ea69a07bee19ef8dbe84be8bfedacc85e18d738
Ruchitha Perla <ruchithaperla@gmail.com>
2026-06-24 21:08:45 +0530

    Added language translation

---
+++ src/i18n.js
@@ -0,0 +1,37 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from './locales/en.json';
+import te from './locales/te.json';
+import hi from './locales/hi.json';
+
+const resources = {
+  English: {
+    translation: en
+  },
+  Telugu: {
+    translation: te
+  },
+  Hindi: {
+    translation: hi
+  }
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'English',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+      lookupLocalStorage: 'i18nextLng'
+    }
+  });
+
+export default i18n;
